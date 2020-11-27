4955a418aa4dd347058f1959b4b9022f8909765c
tofantaraki <tommyliaros@gmail.com>
2020-11-27 16:05:43 +0200

    aed event change type of occurrencePoint

--- aed-service/src/main/java/com/droukos/aedservice/model/aed_event/AedEvent.java
+++ aed-service/src/main/java/com/droukos/aedservice/model/aed_event/AedEvent.java
@@ -2,6 +2,7 @@ package com.droukos.aedservice.model.aed_event;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.geo.GeoJson;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -25,7 +26,7 @@ public class AedEvent {
     @Indexed
     private Integer occurrenceType;
 
-    private GeoJson occurrencePoint;
+    private Point occurrencePoint;
 
     private String address;
 
