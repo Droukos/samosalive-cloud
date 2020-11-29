6deda26bb14f7e3db84654cbef6fdf52cfe80da1
tofantaraki <tommyliaros@gmail.com>
2020-11-29 18:55:15 +0200

    remove cdn service

--- cdn-service/src/main/java/com/droukos/cdnservice/environment/constants/Platforms.java
+++
@@ -1,7 +0,0 @@
-package com.droukos.cdnservice.environment.constants;
-
-public class Platforms {
-    public final static String WEB = "web";
-    public final static String ANDROID = "android";
-    public final static String IOS = "ios";
-}
